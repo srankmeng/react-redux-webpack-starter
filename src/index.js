89f2896113e6bb63282ccae32e700b84f7a8bbb3
meng_jaturong <jaturong.d@dotography.com>
2017-09-10 20:10:21 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import './index.scss'
+
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      <h1 className="greeting">Hello World</h1>
+    )
+  }
+}
+
+render(<HelloWorld />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// const webpack = require('webpack')
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  devtool: 'eval',
+  // entry: './src/index.js',
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    publicPath: '/public/static/',
+    path: path.join(__dirname, 'public', 'static'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              sourceMap: true,
+              localIdentName: '[local]-[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'sass-loader',
+            query: {
+              sourceMap: true,
+              localIdentName: '[local]-[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    inline: true,
+  },
+}
